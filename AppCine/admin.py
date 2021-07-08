5d97d04693aa7f4203e3dd1d71fa609e11d7d91a
Pedro Sanchez <psanchezm@unsa.edu.pe>
2021-07-08 14:28:41 -0500

    Se agregaron algunas clases modelos

--- AppCine/admin.py
+++ AppCine/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
-from.models import UsuarioCliente,Pelicula
+from.models import UsuarioCliente,Pelicula,Sala, Reservas, Promocion, Entrada, Cartelera
 
 admin.site.register(UsuarioCliente)
 admin.site.register(Pelicula)
+admin.site.register(Sala)
+admin.site.register(Reservas)
+admin.site.register(Promocion)
+admin.site.register(Entrada)
+admin.site.register(Cartelera)
--- AppCine/models.py
+++ AppCine/models.py
@@ -2,6 +2,10 @@ from django.db import models
 
 # Create your models here.
 
+#class Sinopsis(models.Model):
+#    descripcion = models.TextField(primary_key=True)
+#    calificacion = models.IntegerField()
+
 class Pelicula(models.Model):
     titulo = models.CharField(max_length=30,primary_key=True)
     gen = [
@@ -19,17 +23,19 @@ class Pelicula(models.Model):
     ]
     idioma = models.CharField(max_length=1,choices=idim)
     #duracion =
-    #sinopsis =  
+    #sinopsis =  models.ForeignKey(Sinopsis, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
         texto = "{0}"
         return texto.format(self.titulo)
 
+
+
 class UsuarioCliente (models.Model):
     IdCliente = models.IntegerField(primary_key=True)
     nombres = models.CharField(max_length=20)
     apellidos = models.CharField(max_length=20)
-    edad = models.IntegerField
+    edad = models.IntegerField()
     pelicula = models.ForeignKey(Pelicula,blank=False,on_delete=models.CASCADE)
     dni = models.CharField(max_length=8)
     correo = models.EmailField(max_length=50)
@@ -42,15 +48,27 @@ class UsuarioCliente (models.Model):
 
 class Sala(models.Model):
     idSala = models.CharField(max_length=3,primary_key=True)
-    capacidad = models.IntegerField
+    capacidad = models.IntegerField()
     
 class Reservas(models.Model):
     #tipo de cliente
     #Pelicula = UsuarioCliente.pelicula
-    hora = models.TimeField
+    hora = models.CharField(max_length=8)
     sala = models.ForeignKey(Sala,blank=False,on_delete=models.CASCADE)
 
 class Promocion(models.Model):
     descuento = models.BooleanField
     #tipo de cliente
 
+class Entrada(models.Model):
+    descripcion = models.TextField()
+    precio = models.IntegerField()
+    #fecha = models.TimeField
+    IdCliente = models.CharField(max_length=10)
+    Promocion = models.BooleanField
+
+class Cartelera(models.Model):
+    peliculas = []
+    #Promocion = BooleanField
+    #sala = models.ForeingKey(Sala, blank=False, on_delete=models.CASCADE)
+    #horario = models.TimeField
