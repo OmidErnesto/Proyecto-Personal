d3fab29231c901ec34997aa1fdba93e1229c8896
OmidErnesto <ochahuaris@unsa.edu.pe>
2021-07-09 22:15:24 -0500

    Pagina Index

--- AppCine/views.py
+++ AppCine/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Pelicula
 
 # Create your views here.
 
 def myHome(request, *args, **kwargs):
+
+    peliculas = Pelicula.objects.all()
     print(args,kwargs)
     print(request.user)
-    return render(request, "home.html", {})
+    return render(request, "home.html", {'peliculas': peliculas})
